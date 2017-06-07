fa4ba99c94eccdf0d6cff53f396b60299ddd3194
lin.peng <lin.peng@all-union.com.cn>
2017-06-07 17:18:49 +0800

    20170607

---
+++ src/components/UserList.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="UserList">
+    <el-button type="text" @click="getLet">UserList</el-button>
+    <div v-model="userlist">{{userlist}}</div>
+  </div>
+</template>
+
+<script>
+  var http = require('http')
+  var options = {
+    host: 'localhost',
+    post: 3000,
+    path: '/dao/list',
+    method: 'GET'
+  }
+  export default {
+    name: 'UserList',
+    methods: {
+      getLet: function () {
+        http.request(options, function (response) {
+          console.log(response)
+          this.userlist = response.toString()
+        })
+      }
+    },
+    data: function () {
+      return {
+        userlist: ''
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
 import Login from '@/components/Login'
+import UserList from '@/components/UserList'
 
 Vue.use(Router)
 
@@ -16,6 +17,11 @@ export default new Router({
       path: '/login',
       name: 'Login',
       component: Login
+    },
+    {
+      path: '/userlist',
+      name: 'UserList',
+      component: UserList
     }
   ]
 })
