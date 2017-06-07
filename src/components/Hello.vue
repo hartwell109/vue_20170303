4830cd9576d142718a6a273105254b93b3df15d6
lin.peng <lin.peng@all-union.com.cn>
2017-06-07 11:26:55 +0800

    20170607

---
+++ src/components/Hello.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="hello">
+    <h2>{{msg}}</h2>
+    <el-carousel :interval="4000" height="600px" arrow="always">
+      <el-carousel-item v-for="item in sliderList" :key="item">
+        <img :src="item.url"></img>
+      </el-carousel-item>
+    </el-carousel>
+    <router-link to="/login">
+      <el-button type="text">Next</el-button>
+    </router-link>
+
+  </div>
+</template>
+
+<script>
+  import ElButton from '../../node_modules/element-ui/packages/button/src/button'
+  export default {
+    components: {ElButton},
+    name: 'hello',
+    data () {
+      return {
+        msg: 'Welcome to Our First Vue.js App',
+        sliderList: [
+          {url: 'http://unsplash.it/800/600', click: '', text: ''},
+          {url: 'http://unsplash.it/800/601', click: '', text: ''},
+          {url: 'http://unsplash.it/800/604', click: '', text: ''},
+          {url: 'http://unsplash.it/800/608', click: '', text: ''}
+        ]
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  h1, h2 {
+    font-weight: normal;
+  }
+
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+
+  a {
+    color: #42b983;
+  }
+</style>
---
+++ src/components/Login.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="Login">
+    <el-row>
+      <h3>用户登陆</h3>
+    </el-row>
+    <el-row :gutter="10">
+      <el-col class="col" :span="2" :offset="10">用户：</el-col>
+      <el-col class="col" :span="2">
+        <el-input v-model="username" placeholder="username"></el-input>
+      </el-col>
+    </el-row>
+    <el-row :gutter="10" align="middle" justify="center">
+      <el-col class="col" :span="2" :offset="10">口令：</el-col>
+      <el-col class="col" :span="2">
+        <el-input v-model="password" placeholder="password"></el-input>
+      </el-col>
+    </el-row>
+    <el-row :gutter="10" align="middle" justify="center">
+      <el-col :span="2" :offset="10">
+        <el-button type="primary" >确定</el-button>
+      </el-col>
+      <el-col :span="2">
+        <el-button type="primary">取消</el-button>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+  export default {
+    emmite: function () {
+      console.log('emmit')
+    },
+    name: 'Login',
+    data: function () {
+      return {
+        username: 'abcd',
+        password: 'abcd'
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .col {
+    padding: 10px 0;
+  }
+</style>
