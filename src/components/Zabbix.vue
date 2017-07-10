0cbc294d843f88646ab147a5a91c6ea1e7c0bc60
lin.peng <lin.peng@all-union.com.cn>
2017-07-10 16:26:46 +0800

    20170710

--- src/components/Zabbix.vue
+++ src/components/Zabbix.vue
@@ -18,7 +18,7 @@
     },
     methods: {
       getData: function (event) {
-        const params = {pageNum: 0}
+        const params = {pageNum: 0, pageSize: 30}
         this.$jsonp('http://localhost:3000/dao/list', params, (err, data) => {
           if (err) {
             console.log(err)
