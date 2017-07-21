567064660e3956d7ea79b0b4d52f8082f1c771c6
lin.peng <lin.peng@all-union.com.cn>
2017-07-21 17:30:57 +0800

    20170720

--- test/e2e/specs/test.js
+++ test/e2e/specs/test.js
@@ -2,18 +2,23 @@
 // http://nightwatchjs.org/guide#usage
 
 module.exports = {
-  'default e2e tests': function (browser) {
-    // automatically uses dev Server port from /config.index.js
-    // default: http://localhost:8080
-    // see nightwatch.conf.js
-    const devServer = browser.globals.devServerURL
-
+  'Step 1': function (browser) {
     browser
-      .url(devServer)
-      .waitForElementVisible('#app', 5000)
-      .assert.elementPresent('.hello')
-      .assert.containsText('h1', 'Welcome to Your Vue.js App')
-      .assert.elementCount('img', 1)
-      .end()
+      .url('http://202.99.45.97:9052/CIS-CHSSF/login')
+      .waitForElementPresent('img[name="captcha"]', 1000)
+  },
+  'Step 2': function (client) {
+    console.log('step 2............')
+    var range = {}
+    client
+      .getElementSize('img[name="captcha"]', (result) => {
+        range.height = result.value.height
+        range.width = result.value.width
+      })
+      .getLocation('img[name="captcha"]', (result) => {
+        range.x = result.value.x
+        range.y = result.value.y
+        console.log(range)
+      })
   }
 }
